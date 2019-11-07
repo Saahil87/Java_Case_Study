80ea8cc61a7b1e66ce68476979bd3635e1903a8e
Saahil Hiranandani <37094544+Saahil87@users.noreply.github.com>
2019-11-07 16:50:23 +0530

    Delete LoginService.java

--- InventoryManagementSystem_Template/build/classes/com/accenture/lkm/services/LoginService.java
+++
@@ -1,8 +0,0 @@
-package com.accenture.lkm.services;
-
-import com.accenture.lkm.business.bean.LoginBean;
-
-public interface LoginService {
-
-	public LoginBean validateLogin(LoginBean loginBean);
-}
